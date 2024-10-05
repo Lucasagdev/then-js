407d51a4cf8db8ad2dd651c0fbbd1e29a5e899ae
Lucas Gomes <lucasagostini400@gmail.com>
2024-10-05 19:02:08 -0300

    Aprendendo a usar a tag Then

---
+++ then.js
@@ -0,0 +1,16 @@
+let chaleiraEstaNoFogao = true
+let fogaoEstaLigado = true
+
+ferverAgua(chaleiraEstaNoFogao, fogaoEstaLigado)
+    .then(() => {
+        return passarOCafe();
+    })
+    .then(() => {
+        return tomarCafe();
+    })
+    .then(() => {
+        return lavarXicara();
+    })
+    .then(() => {
+        return console.log('Finalizando ritual do café, bora trabalhar');
+    });
